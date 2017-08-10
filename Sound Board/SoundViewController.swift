05fcd997c7cfb9e6ea33006d87fe51a6e1e0cbc5
Jef Cunningham <jef@snowball.is>
2017-08-09 22:44:02 -0700

    added sound recording (lecture 92)

--- Sound Board/SoundViewController.swift
+++ Sound Board/SoundViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import AVFoundation
 
 class SoundViewController: UIViewController {
-
+    
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var nameTextField: UITextField!
     
@@ -18,35 +18,41 @@ class SoundViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         setupRecorder()
     }
-
+    
     func setupRecorder() {
+        // because you have these session things below they have to have try before them, 
+        // when you have try you must wrap them all in a do-catch thingy
         do {
             //Create an audio session
             let session = AVAudioSession.sharedInstance()
+            // you have to put 'try' in front of these and then wrap the entire thing in a 'do {} catch {}' thingy
             try session.setCategory(AVAudioSessionCategoryPlayAndRecord)
             try session.overrideOutputAudioPort(.speaker)
             try session.setActive(true)
-        
-            // Create URL for the audio file
             
+            // Create URL for the audio file
+            // how to get ot the app's sandboxed documents directory
             let basePath : String = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
             let pathComponents = [basePath, "audio.m4a"]
             let audioURL = NSURL.fileURL(withPathComponents: pathComponents)!
-        
-            // Create settings for the audio recorder
             
+            // print out the url of where we are storing the audio recording
+            print("###############")
+            print(audioURL)
+            print("###############")
+            
+            // Create settings for the audio recorder
             var settings : [String:Any] = [:]
             settings[AVFormatIDKey] = Int(kAudioFormatMPEG4AAC)
             settings[AVSampleRateKey] = 44100.0
             settings[AVNumberOfChannelsKey] = 2
             
-        
+            
             // create audiorecorder object
-        
             audioRecorder = try AVAudioRecorder(url: audioURL, settings: settings)
             audioRecorder!.prepareToRecord()
             
@@ -57,6 +63,17 @@ class SoundViewController: UIViewController {
     
     
     @IBAction func recordTapped(_ sender: Any) {
+        if audioRecorder!.isRecording {
+            // stop the recording
+            audioRecorder?.stop()
+            // chenge the button title to record
+            recordButton.setTitle("Record", for: .normal)
+        } else {
+            // start the recording
+            audioRecorder?.record()
+            // change the button title to stop
+            recordButton.setTitle("Stop", for: .normal)
+        }
     }
     
     @IBAction func playTapped(_ sender: Any) {
@@ -65,4 +82,4 @@ class SoundViewController: UIViewController {
     @IBAction func addTapped(_ sender: Any) {
     }
     
-   }
+}
