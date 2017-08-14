3af25fd6167a0f0cfaf8842986c473612e1af3e4
Jef Cunningham <jef@snowball.is>
2017-08-13 20:37:43 -0700

    Adding infoplist entry asking user for mic use

---
+++ Build/Intermediates/Sound Board.build/Debug-iphoneos/Sound Board.build/DerivedSources/CoreDataGenerated/Sound_Board/Sound_Board+CoreDataModel.swift
@@ -0,0 +1,13 @@
+//
+//  Sound_Board+CoreDataModel.swift
+//  
+//
+//  Created by Jef Cunningham on 8/13/17.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+import Foundation
+import CoreData
+
+
